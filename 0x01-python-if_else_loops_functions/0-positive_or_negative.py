3895761bad98b07769b90f7c0407a45baeacdbbe
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2019-09-10 12:40:18 -0500

    0x00: refactor, deletion of old folder

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -2,6 +2,6 @@
 import random
 number = random.randint(-10, 10)
 print("{:d} is".format(number),
-      "zero" if number == 0 else
-      "positive" if number > 0
+      "zero" if number == 0
+      else "positive" if number > 0
       else "negative")
--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,5 +4,5 @@ number = random.randint(-10000, 10000)
 print("Last digit of {:d} is".format(number),
       "{:d} and is".format(number % 10),
       "0" if number % 10 == 0 else
-      "greater than five" if number % 10 > 5
+      "greater than 5" if number % 10 > 5
       else "less than 6 and not 0")
