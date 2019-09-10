3c1ecb4aa5999eac35640fff666edf725e7e4451
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2019-09-09 19:22:56 -0500

    0x00: refactor

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,17 +1,23 @@
 #include "lists.h"
 
+/**
+ * check_cycle - checks if a linked list has a cycle
+ * @list: head of the list
+ *
+ * Return: if the linked list has a cycle, the function will return 1. Else
+ * the function will return 0.
+ */
 int check_cycle(listint_t *list)
 {
-	listint_t *slow = list;	
-	listint_t *fast = list->next;
+	listint_t *slow = list;
+	listint_t *fast = list;
 
-	while(slow != fast)
-	{
+	do {
 		if (!fast->next || !fast->next->next)
 			break;
 		slow = slow->next;
 		fast = fast->next->next;
-	}
+	} while (slow != fast);
 
 	if (slow == fast)
 		return (1);
