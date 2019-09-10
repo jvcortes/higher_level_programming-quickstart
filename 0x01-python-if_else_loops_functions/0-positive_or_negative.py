7328f9375c010dcf3b50e195e5d9a4968ca30166
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2019-09-10 12:37:53 -0500

    0x01: refactor on tasks 0 and 1

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10, 10)
-print("{:d} is".format(number), "zero" if number == 0 else
-      "positive" if number > 0 else "negative")
+print("{:d} is".format(number),
+      "zero" if number == 0 else
+      "positive" if number > 0
+      else "negative")
--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,6 +3,6 @@ import random
 number = random.randint(-10000, 10000)
 print("Last digit of {:d} is".format(number),
       "{:d} and is".format(number % 10),
-      "zero" if number % 10 == 0 else
+      "0" if number % 10 == 0 else
       "greater than five" if number % 10 > 5
       else "less than 6 and not 0")
