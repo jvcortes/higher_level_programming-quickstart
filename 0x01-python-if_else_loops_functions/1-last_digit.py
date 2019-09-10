16ab922e7265873fe52f096fafedb6a3eb827d34
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2019-09-10 13:08:34 -0500

    0x01: fix on task 1
    Fixed use case with negative numbers

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-print("Last digit of {:d} is".format(number),
-      "{:d} and is".format(number % 10),
-      "0" if number % 10 == 0 else
-      "greater than 5" if number % 10 > 5
-      else "less than 6 and not 0")
+if (number % 10 if number > 0
+        else (number * -1) % 10) == 0:
+    print("Last digit of {:d} is {:d} and is zero"
+          .format(number, number % 10 if number > 0
+                  else (number * -1) % 10))
+elif (number % 10 if number > 0
+      else (number * -1) % 10) > 5:
+    print("Last digit of {:d} is {:d} and is greater than 5"
+          .format(number, number % 10 if number > 0
+                  else (number * -1) % 10))
+else:
+    print("Last digit of {:d} is {:d} and is less than 6 and not 0"
+          .format(number, number % 10 if number > 0
+                  else (number * -1) % 10))
