8d7660ccf764a89d26cfe817c869ebef2b45624e
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2019-09-17 21:28:44 -0500

    (0x03) task 4
    Created script file for task 4.

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if 0 <= idx <= len(my_list) - 1:
+        copy = my_list.copy()
+        copy[idx] = element
+        return copy
+    return my_list
