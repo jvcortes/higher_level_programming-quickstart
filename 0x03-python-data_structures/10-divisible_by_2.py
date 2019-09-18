c222446e83cd9557099b345ac863b38fddf85ab5
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2019-09-17 22:52:44 -0500

    (0x03)(fix) task 10
    Fixed use case for empty lists.

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
+    is_div = []
     if my_list:
-        is_div = []
         for i in my_list:
             if not abs(i) % 2:
                 is_div.append(True)
             else:
                 is_div.append(False)
-        return is_div
+    return is_div
+
