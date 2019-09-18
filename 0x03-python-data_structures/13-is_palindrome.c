9dbd4a9cee1284312c5b6e96530f0d54282a2239
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2019-09-18 15:45:30 -0500

    (0x03)(optimizaion) task 13
    Removed use of malloc.

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include "lists.h"
 
+#define SIZE 1024
+
 /**
  * is_palindrome - checks if a listint_t linked list is a palindrome
  * @head: list head
@@ -10,20 +12,12 @@
 int is_palindrome(listint_t **head)
 {
 	listint_t *current = *head;
-	int *n;
+	int n[SIZE];
 	int i = 0, j, size;
 
 	if (!*head)
 		return (1);
 
-	while (current)
-	{
-		current = current->next;
-		i++;
-	}
-	current = *head;
-
-	n = malloc(sizeof(int) * i);
 	i = 0;
 	while (current)
 	{
@@ -35,11 +29,7 @@ int is_palindrome(listint_t **head)
 
 	for (i = 0, j = size - 1; i < size && j >= 0; i++, j--)
 		if (n[i] != n[j])
-		{
-			free(n);
 			return (0);
-		}
 
-	free(n);
 	return (1);
 }
