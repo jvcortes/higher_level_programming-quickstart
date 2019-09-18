aa2c598901d520181682576a302a95e9f603e839
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2019-09-18 17:15:24 -0500

    (0x03)(fix): task 13
    Removed use of arrays and malloc.

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -9,35 +9,36 @@
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *current = *head;
-	int *n;
-	int i = 0, j = 0, palindrome = 1;
+	listint_t *top = *head;
+	listint_t *bottom = *head;
+	int i = 0, j = 0;
 
 	if (!*head)
 		return (1);
 
-	while (current)
+	while (bottom->next)
 	{
-		current = current->next;
+		bottom = bottom->next;
 		i++;
 	}
-	current = *head;
 
-	n = malloc(sizeof(int) * i);
-	i = 0;
-	while (current)
+	i--;
+	while (top)
 	{
-		n[i++] = current->n;
-		current = current->next;
+		j = 0;
+		if (top->n != bottom->n)
+			break;
+		else if (bottom == *head)
+			return (1);
+		top = top->next;
+		i--;
+		bottom = *head;
+		while (j <= i)
+		{
+			bottom = bottom->next;
+			j++;
+		}
 	}
 
-	for (j = i - 1, i = 0; j >= 0; i++, j--)
-		if (n[i] != n[j])
-		{
-			palindrome = 0;
-			break;
-		}
-
-	free(n);
-	return (palindrome);
+	return (0);
 }
