629517312921d32948b206d28954c794357e4784
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2019-09-30 15:45:21 -0500

    (0x05) initial commit

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,10 @@
+def safe_print_list(my_list=[], x=0):
+    c = 0
+    for i in my_list[:x]:
+        try:
+            print("{}".format(i), end='')
+            c += 1
+        except IndexError:
+            break
+    print()
+    return c
