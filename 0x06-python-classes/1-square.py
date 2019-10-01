a7aedf8e42f737f79883aad93c51c498649e00ed
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2019-10-01 18:55:03 -0500

    (0x06) task 1
    Created class definition for task 1.

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+
+This module contains the Square class
+
+"""
+
+class Square:
+    """ Defines a Square class
+
+    Args:
+        size (int): Initial size of the square
+
+    Attributes:
+        size (int): Size of the square
+
+    """
+    def __init__(self, size):
+        """ Initializes a Square type object
+
+        Args:
+            size (int): Initial size of the square
+
+        """
+        self.__size = size
