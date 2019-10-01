84a6c87d4ab10722e7f94997dcf37174e1c14812
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2019-10-01 18:42:09 -0500

    (0x06) task 0
    Created class definition for task 0.

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+
+This module contains the empty Square class
+
+"""
+
+
+class Square:
+    """ Defines an empty Square class"""
+    pass
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,3 @@
+# 0x06: Python - classes
+
+TODO: content list
