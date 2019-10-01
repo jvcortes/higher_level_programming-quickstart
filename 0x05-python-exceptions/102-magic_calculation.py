7019755d46719dfb694cff29b37714a604730ef8
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2019-09-30 22:25:10 -0500

    (0x05: refactor) pep8 1.7.1

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -7,7 +7,7 @@ def magic_calculation(a, b):
                 raise Exception('Too far')
             else:
                 result += (a ** b) / i
-        except: # A bare except is also dangerous.
+        except:  # A bare except is also dangerous.
             result = a + b
             break
     return result
