779ef957436a0ce63040168a4040ecbc35bd41ae
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2019-10-02 00:03:21 -0500

    (0x06: fix) task 10
    added docstrings

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -3,19 +3,19 @@ import math
 
 
 class MagicClass:
-    """ Defines a circle object """
+    """ Defines a circle object."""
 
     def __init__(self, radius):
-
+        """ Initializes the circle object. """
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError("radius must be a number")
         self.__radius = radius
 
     def area(self):
-
+        """ Returns the area of the circle."""
         return (self.__radius ** 2) * math.pi
 
     def circumference(self):
-
+        """ Returns the circumference of the circle."""
         return (2 * math.pi) * self.__radius
