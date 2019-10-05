277cba1a6bc703041bc2d81ffb07dcce2ce07a58
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2019-10-04 21:01:03 -0500

    (0x07) task 2
    Created test cases and function definition for task 2.

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+
+This module contains the function definition for task 2.
+
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """ Prints "My name is <first_name> <last_name>".
+
+    Args:
+        first_name (str): First name.
+        last_name (str): Last name.
+    """
+
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    print("My name is {}".format(first_name), end='')
+    if last_name:
+        print(" {}".format(last_name))
