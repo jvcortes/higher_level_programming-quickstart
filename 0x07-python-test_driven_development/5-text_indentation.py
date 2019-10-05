f8a61726bb3dd0468bf20eddabc7f9d05414e12a
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2019-10-04 23:36:25 -0500

    (0x07) task 5
    Created test cases and function definition for task 5.

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+
+This module contains the function definition for task 5.
+
+"""
+
+
+def text_indentation(text):
+    """Prints a text with two new line characters after:
+        - A period character (``.``).
+        - A colon character (``:``).
+        - A question mark character (``?``)
+
+    Args:
+        text (str): text to print
+    """
+    result = ""
+    if isinstance(text, str):
+        if text:
+            for c in text:
+                result += c
+                if c in ('?', '.', ':'):
+                    result = result.strip(" ")
+                    print(result, end='\n\n')
+                    result = ""
+            result = result.strip(" ")
+            print(result, end='')
