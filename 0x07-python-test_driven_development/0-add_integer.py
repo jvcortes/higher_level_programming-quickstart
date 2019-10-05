484c38a7e95ce7c7701ecae72a825709dc1e4c5f
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2019-10-05 13:32:00 -0500

    (0x07: fix) task 0
    Fixed wrong behavior of function add_integer, the function will now
    return a rounded integer.

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -7,18 +7,14 @@ This module contains the function definition for task 0.
 
 
 def add_integer(a, b=98):
-    """ Returns the sum of two provided numbers.
+    """ Returns the integer sum of two provided numbers.
 
     Args:
         a (int, float): First number.
         b (int, float): Second number (default: 98)
     """
-    if isinstance(a, (int, float)):
-        result = a
-    else:
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if isinstance(b, (int, float)):
-        result += b
-    else:
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    return result
+    return int(round(a + b))
