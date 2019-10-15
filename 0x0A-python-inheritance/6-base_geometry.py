72e2473eefa33dd3d24f17eac068801c31e63aa6
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2019-10-15 17:48:50 -0500

    (0x0A) task 6
    Created class definition for task 6.

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+
+This module holds the function definition for task 0
+
+"""
+
+
+class BaseGeometry():
+    """ Defines an empty Geometry object. """
+
+    def area(self):
+        raise Exception("area() is not implemented")
