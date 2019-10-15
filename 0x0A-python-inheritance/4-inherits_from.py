7a24f08be8aa72110bf4126701b34fcfa3a7942e
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2019-10-15 15:56:24 -0500

    (0x0A) task 4
    Created function definition for task 4.

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+
+This module holds the function definition for task 0
+
+"""
+
+
+def inherits_from(obj, a_class):
+    """Checks if an object extends from a specified class.
+
+    Args:
+        obj (object): provided object
+        a_class (object): provided class
+    """
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
