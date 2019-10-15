c29ea4726a21a1833b1000237a8886ebbfe75ddf
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2019-10-15 14:34:47 -0500

    (0x0A) initial commit

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+
+This module holds the function definition for task 0
+
+"""
+
+
+def lookup(obj):
+    """Returns a list with the attributes and methods of an object. """
+    return dir(obj)
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,3 @@
+# 0x0A: Python - class inheritance
+
+TODO: content list
