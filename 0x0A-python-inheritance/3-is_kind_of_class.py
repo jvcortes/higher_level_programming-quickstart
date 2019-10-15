008142e36827b4f67ccccec2b07fa194c54b96ae
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2019-10-15 15:02:27 -0500

    (0x0A) task 3
    Created function definition for task 3.

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+
+This module holds the function definition for task 3
+
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """ Checks if an object is an instance of a specified object.
+
+    Args:
+        obj (object): provided object
+        a_class (object): provided class
+    """
+
+    return isinstance(obj, a_class)
