9fc6bb47f2b637c56daa29fd99fddc3f1a21f7de
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2019-10-15 19:16:06 -0500

    (0x0A) task 11
    Created class definition for task 11.

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+
+This module holds the class definition for task 10
+
+"""
+"""
+perhaps i should import the module before declaring the class.
+"""
+
+
+class Square(__import__("9-rectangle").Rectangle):
+    """ Defines a square.
+
+        Args:
+            size (int): size of the square
+
+        Attributes:
+            size (int): size of the square
+    """
+
+    def __init__(self, size):
+        """ Instanciates a Rectangle object.
+
+        Args:
+            square (int): size of the square, must be greater than zero.
+        """
+
+        self.integer_validator("size", size)
+
+        self.__size = size
+
+    def area(self):
+        """ Returns the area of the shape. """
+        return self.__size ** 2
+
+    def __str__(self):
+        """ Returns the string representation of the square. """
+        return "[{}] {}/{}".format(self.__class__.__name__,
+                                   self.__size,
+                                   self.__size)
