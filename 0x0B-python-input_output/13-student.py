b0eb827e039fca5c806649ebe560b4de90235f56
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2019-10-16 22:19:22 -0500

    (0x0B) task 13
    Created class definition for task 13.

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""
+This module contains the class definition for task 13.
+"""
+
+
+class Student:
+    """ Defines a student
+
+    Args:
+        first_name (str): student's first name
+        last_name (str): student's last name
+        age (int): student's age
+
+    Attributes:
+        first_name (str): student's first name
+        last_name (str): student's last name
+        age (int): student's age
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """ Initializes a Student instance
+
+        Args:
+            first_name (str): student's first name
+            last_name (str): student's last name
+            age (int): student's age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Returns the Student instance's dictionary.
+
+        Args:
+            attrs (list: str): list of attributes to retrieve, if the list is
+            empty, the function will retrieve all the attributes.
+        """
+
+        if attrs is None or attrs == 0:
+            return self.__dict__
+        return dict(filter(lambda x: x[0] in attrs, self.__dict__.items()))
+
+    def reload_from_json(self, json):
+        """ Replaces all the attributes of the Student's instance from a JSON
+            representation.
+
+        Args:
+            json (str): JSON representation
+        """
+
+        self.__dict__ = json
