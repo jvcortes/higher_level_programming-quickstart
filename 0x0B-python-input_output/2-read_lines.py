68128e79c39084c17797bab3c33d271141429c33
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2019-10-16 19:46:41 -0500

    (0x0B) task 2
    Created function definition for task 2.

---
+++ 0x0B-python-input_output/2-read_lines.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This module contains the function definition for task 2.
+"""
+
+
+def read_lines(filename="", nb_lines=0):
+    """ reads and prints the first n lines of a file
+
+    Args:
+        filename (str): file name
+        nb_lines (int): number of lines
+    """
+    with open(filename, 'r') as f:
+        for n, line in enumerate(f):
+            if n >= nb_lines:
+                break
+            print(line, end='')
