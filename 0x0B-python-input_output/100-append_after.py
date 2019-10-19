71323c041168fd9cbdee3cd2367ce65421bd89ae
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2019-10-18 21:04:04 -0500

    (0x0B) task 15
    Created function definition for task 15.

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+This module contains the function definition for task 15.
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """ appends a new string after a the lines that contain a
+    specified string inside a file.
+
+    Args:
+        filename (str): name of the JSON file
+        search_string (str): string to search
+        new_string (str): string to append
+    """
+    with open(filename, "r+") as f:
+        content = f.readlines()
+        new_content = ""
+        for line in content:
+            new_line = line
+            if search_string in line:
+                new_line += new_string
+            new_content += new_line
+        print(new_content)
+        f.truncate(0)
+        f.seek(0)
+        f.write(new_content)
