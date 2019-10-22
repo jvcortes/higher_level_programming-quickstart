485c8eeff730b394d2f32abd64ec80e78a28cb0a
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2019-10-21 19:57:00 -0500

    (0x0C) task 3
    Created class definition and test cases for task 3.

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -30,6 +30,24 @@ class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         super(Rectangle, self).__init__(id)
 
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if not isinstance(x, int):
+            raise TypeError("x must be an integer")
+        if not isinstance(y, int):
+            raise TypeError("y must be an integer")
+
+        if width <= 0:
+            raise ValueError("width must be > 0")
+        if height <= 0:
+            raise ValueError("height must be > 0")
+        if x < 0:
+            raise ValueError("x must be >= 0")
+        if y < 0:
+            raise ValueError("y must be >= 0")
+
         self.__width = width
         self.__height = height
         self.__x = x
@@ -38,10 +56,17 @@ class Rectangle(Base):
     @property
     def width(self):
         """int: width of the Rectangle instance"""
+
         return self.__width
 
     @width.setter
     def width(self, value):
+
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+
         self.__width = value
 
     @property
@@ -51,6 +76,12 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
+
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+
         self.__height = value
 
     @property
@@ -60,6 +91,12 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
+
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+
         self.__x = value
 
     @property
@@ -69,4 +106,10 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
+
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+
         self.__y = value
--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -46,23 +46,265 @@ class RectangleTest(unittest.TestCase):
         self.assertEqual(instance.x, 0, "wrong x")
         self.assertEqual(instance.y, 0, "wrong y")
 
+    def test_try_to_create_instance_with_width_as_floating_point(self):
+        with self.assertRaises(TypeError) as context:
+            Rectangle(2.1, 2)
+
+        self.assertTrue('width must be an integer'
+                        in str(context.exception))
+
+    def test_try_to_create_instance_with_width_as_string(self):
+        with self.assertRaises(TypeError) as context:
+            Rectangle("100", 2)
+
+        self.assertTrue('width must be an integer'
+                        in str(context.exception))
+
+    def test_try_to_create_instance_with_width_as_list(self):
+        with self.assertRaises(TypeError) as context:
+            Rectangle([10, 20], 2)
+
+        self.assertTrue('width must be an integer'
+                        in str(context.exception))
+
+    def test_try_to_create_instance_with_height_as_floating_point(self):
+        with self.assertRaises(TypeError) as context:
+            Rectangle(2, 3.14)
+
+        self.assertTrue('height must be an integer'
+                        in str(context.exception))
+
+    def test_try_to_create_instance_with_height_as_string(self):
+        with self.assertRaises(TypeError) as context:
+            Rectangle(100, "3")
+
+        self.assertTrue('height must be an integer'
+                        in str(context.exception))
+
+    def test_try_to_create_instance_with_height_as_list(self):
+        with self.assertRaises(TypeError) as context:
+            Rectangle(20, [1, 2])
+
+        self.assertTrue('height must be an integer'
+                        in str(context.exception))
+
+    def test_try_to_create_instance_with_x_as_floating_point(self):
+        with self.assertRaises(TypeError) as context:
+            Rectangle(2, 2, 1.0)
+
+        self.assertTrue('x must be an integer'
+                        in str(context.exception))
+
+    def test_try_to_create_instance_with_x_as_string(self):
+        with self.assertRaises(TypeError) as context:
+            Rectangle(2, 2, "1")
+
+        self.assertTrue('x must be an integer'
+                        in str(context.exception))
+
+    def test_try_to_create_instance_with_x_as_list(self):
+        with self.assertRaises(TypeError) as context:
+            Rectangle(20, 2, [1])
+
+        self.assertTrue('x must be an integer'
+                        in str(context.exception))
+
+    def test_try_to_create_instance_with_y_as_floating_point(self):
+        with self.assertRaises(TypeError) as context:
+            Rectangle(2, 2, 1, 12.3)
+
+        self.assertTrue('y must be an integer'
+                        in str(context.exception))
+
+    def test_try_to_create_instance_with_y_as_string(self):
+        with self.assertRaises(TypeError) as context:
+            Rectangle(2, 2, 1, "2")
+
+        self.assertTrue('y must be an integer'
+                        in str(context.exception))
+
+    def test_try_to_create_instance_with_y_as_list(self):
+        with self.assertRaises(TypeError) as context:
+            Rectangle(20, 2, 1, [5, 2])
+
+        self.assertTrue('y must be an integer'
+                        in str(context.exception))
+
+    def test_try_to_create_instance_with_a_negative_width(self):
+        with self.assertRaises(ValueError) as context:
+            Rectangle(-2, 2)
+
+        self.assertTrue('width must be > 0'
+                        in str(context.exception))
+
+    def test_try_to_create_instance_with_a_zero_width(self):
+        with self.assertRaises(ValueError) as context:
+            Rectangle(0, 2)
+
+        self.assertTrue('width must be > 0'
+                        in str(context.exception))
+
+    def test_try_to_create_instance_with_a_negative_height(self):
+        with self.assertRaises(ValueError) as context:
+            Rectangle(2, -2)
+
+        self.assertTrue('height must be > 0'
+                        in str(context.exception))
+
+    def test_try_to_create_instance_with_a_zero_height(self):
+        with self.assertRaises(ValueError) as context:
+            Rectangle(2, 0)
+
+        self.assertTrue('height must be > 0'
+                        in str(context.exception))
+
+    def test_try_to_create_instance_with_negative_x(self):
+        with self.assertRaises(ValueError) as context:
+            Rectangle(2, 5, -1)
+
+        self.assertTrue('x must be >= 0'
+                        in str(context.exception))
+
+    def test_try_to_create_instance_with_negative_y(self):
+        with self.assertRaises(ValueError) as context:
+            Rectangle(2, 5, 1, -1)
+
+        self.assertTrue('y must be >= 0'
+                        in str(context.exception))
+
+    def test_set_instance_member_height(self):
+        instance = Rectangle(2, 2)
+        instance.height = 1
+        self.assertEqual(instance.height, 1, "wrong height")
+
+    def test_set_instance_member_height_to_string(self):
+        with self.assertRaises(TypeError) as context:
+            instance = Rectangle(2, 2)
+            instance.height = "NUMBER"
+
+        self.assertTrue('height must be an integer'
+                        in str(context.exception))
+
+    def test_set_instance_member_height_to_list(self):
+        with self.assertRaises(TypeError) as context:
+            instance = Rectangle(2, 2)
+            instance.height = [1, 2]
+
+        self.assertTrue('height must be an integer'
+                        in str(context.exception))
+
+    def test_set_instance_member_height_to_float(self):
+        with self.assertRaises(TypeError) as context:
+            instance = Rectangle(2, 2)
+            instance.height = 2.2
+
+        self.assertTrue('height must be an integer'
+                        in str(context.exception))
+
+    def test_set_instance_member_width(self):
+        instance = Rectangle(2, 2)
+        instance.width = 1
+        self.assertEqual(instance.width, 1, "wrong width")
+
+    def test_set_instance_member_width_to_string(self):
+        with self.assertRaises(TypeError) as context:
+            instance = Rectangle(2, 2)
+            instance.width = "NUMBER"
+
+        self.assertTrue('width must be an integer'
+                        in str(context.exception))
+
+    def test_set_instance_member_width_to_list(self):
+        with self.assertRaises(TypeError) as context:
+            instance = Rectangle(2, 2)
+            instance.width = [1, 2]
+
+        self.assertTrue('width must be an integer'
+                        in str(context.exception))
+
+    def test_set_instance_member_width_to_float(self):
+        with self.assertRaises(TypeError) as context:
+            instance = Rectangle(2, 2)
+            instance.width = 2.2
+
+        self.assertTrue('width must be an integer'
+                        in str(context.exception))
+
+    def test_set_instance_member_x(self):
+        instance = Rectangle(2, 2)
+        instance.x = 1
+        self.assertEqual(instance.x, 1, "wrong x")
+
+    def test_set_instance_member_x_to_string(self):
+        with self.assertRaises(TypeError) as context:
+            instance = Rectangle(2, 2)
+            instance.x = "NUMBER"
+
+        self.assertTrue('x must be an integer'
+                        in str(context.exception))
+
+    def test_set_instance_member_x_to_list(self):
+        with self.assertRaises(TypeError) as context:
+            instance = Rectangle(2, 2)
+            instance.x = [1, 2]
+
+        self.assertTrue('x must be an integer'
+                        in str(context.exception))
+
+    def test_set_instance_member_x_to_float(self):
+        with self.assertRaises(TypeError) as context:
+            instance = Rectangle(2, 2)
+            instance.x = 2.2
+
+        self.assertTrue('x must be an integer'
+                        in str(context.exception))
+
+    def test_set_instance_member_y(self):
+        instance = Rectangle(2, 2)
+        instance.y = 1
+        self.assertEqual(instance.y, 1, "wrong y")
+
+    def test_set_instance_member_y_to_string(self):
+        with self.assertRaises(TypeError) as context:
+            instance = Rectangle(2, 2)
+            instance.y = "NUMBER"
+
+        self.assertTrue('y must be an integer'
+                        in str(context.exception))
+
+    def test_set_instance_member_y_to_list(self):
+        with self.assertRaises(TypeError) as context:
+            instance = Rectangle(2, 2)
+            instance.y = [1, 2]
+
+        self.assertTrue('y must be an integer'
+                        in str(context.exception))
+
+    def test_set_instance_member_y_to_float(self):
+        with self.assertRaises(TypeError) as context:
+            instance = Rectangle(2, 2)
+            instance.y = 2.2
+
+        self.assertTrue('y must be an integer'
+                        in str(context.exception))
+
     def test_try_to_create_instance_with_no_args(self):
         with self.assertRaises(TypeError) as context:
-            instance = Rectangle()
+            Rectangle()
 
         self.assertTrue('__init__() missing 2 required positional arguments'
                         in str(context.exception))
 
     def test_try_to_create_instance_with_only_one_arg(self):
         with self.assertRaises(TypeError) as context:
-            instance = Rectangle(1)
+            Rectangle(1)
 
         self.assertTrue('__init__() missing 1 required positional argument'
                         in str(context.exception))
 
     def test_try_to_create_instance_with_additional_args(self):
         with self.assertRaises(TypeError) as context:
-            instance = Rectangle(1, 2, 3, 4, 5, 6)
+            Rectangle(1, 2, 3, 4, 5, 6)
 
         self.assertTrue('__init__() takes from 3 to 6 positional arguments'
                         in str(context.exception))
