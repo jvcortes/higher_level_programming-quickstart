626c75175098418ac5240c09919c094afdf0f304
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2019-10-21 20:29:12 -0500

    (0x0C) new test for Rectangle.area()

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_area_rectangle.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+"""
+This module contains the unit tests for the `Base` class.
+"""
+
+
+class BaseTest(unittest.TestCase):
+    """
+    Defines the test cases for the `Base` class
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        for name in Base.__dict__:
+            if "__nb_objects" in name:
+                setattr(Base, name, 0)
+
+    def test_area(self):
+        instance = Rectangle(4, 5)
+        self.assertEqual(instance.area(), 20)
+
+    def test_area_big_number(self):
+        instance = Rectangle(4444444444444444444444, 1111111111111111111111111)
+        self.assertEqual(instance.area(), 4444444444444444444444 *
+                         1111111111111111111111111)
+
+    def test_area_args(self):
+        with self.assertRaises(TypeError) as context:
+            instance = Rectangle(1, 4)
+            instance.area(1)
+
+        self.assertTrue('area() takes 1 positional argument but 2 were given'
+                        in str(context.exception))
