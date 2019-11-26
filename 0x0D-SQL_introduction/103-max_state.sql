4834057fc4aac02a7fd1607c4ba22802c4e4cee1
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2019-11-26 07:42:15 -0500

    (0x0D, fix) task 20
    Corrected expected output for the SQL script 103-max_state.sql

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,4 +1,4 @@
 -- Shows the max temperature of each state, ordered by the state name.
-SELECT state, max(value) as max_value FROM temperatures
+SELECT state, max(value) as max_temp FROM temperatures
 	GROUP BY state
 	ORDER BY state ASC;
