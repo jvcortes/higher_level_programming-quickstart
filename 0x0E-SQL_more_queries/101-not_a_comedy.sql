1834a6e1174f51e8dbb2f857beae1448323feff8
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2019-11-26 18:25:12 -0500

    (0x0E) task 18
    Created SQL script file for task 18.

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,9 @@
+-- Lists the shows whose genre is not 'Comedy'
+SELECT tv_shows.title FROM ((tv_genres
+	RIGHT JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id)
+	RIGHT JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id)
+	WHERE tv_shows.title NOT IN 
+		(SELECT tv_shows.title FROM ((tv_genres
+                        INNER JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id)
+                  	INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id)
+                        WHERE tv_genres.name = 'Comedy')
