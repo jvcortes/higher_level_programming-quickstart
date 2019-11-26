6a8b584eec5fe1a0169b599aedd5800420635e1b
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2019-11-25 23:35:50 -0500

    (0x0D) task 4
    Created SQL script file for task 4.

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- Creates a table with the name 'first_table' in the current database.
+-- The table will contain an id INT and a name VARCHAR(256)
+CREATE TABLE first_table (
+	id INT,
+	name VARCHAR(256)
+);
