159435aff5501e82273e34e86e494d6aa0c6f7ec
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2019-11-26 11:27:30 -0500

    (0x0E) task 7
    Created SQL script file for task 7.

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,12 @@
+-- Creates a database 'hbtn_0d_usa'
+-- Creates a table 'cities' inside 'hbtn_0d_usa' with the following properties:
+-- id, INT, auto generated, not null, primary key
+-- state_id, INT, foreign key that references 'id' from the 'states' table
+-- name, VARCHAR(256) not null
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE cities (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	state_id INT NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	FOREIGN KEY (`state_id`) REFERENCES `states`(`id`)
+)
