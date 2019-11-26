7a93729f40935485da4933136ec5916b92ba67ce
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2019-11-26 09:38:53 -0500

    (0x0E) initial commit

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,3 @@
+-- Shows the grants for the users 'user_0d_1' and 'user_0d_2' in the current server.
+SHOW GRANTS for 'user_0d_1'@'localhost';
+SHOW GRANTS for 'user_0d_2'@'localhost';
---
+++ 0x0E-SQL_more_queries/README.md
@@ -0,0 +1,3 @@
+# 0x0E: SQL mode queries
+
+TODO: content list
