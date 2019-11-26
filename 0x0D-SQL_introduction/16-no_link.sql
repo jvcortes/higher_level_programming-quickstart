14d9668f6814057cfbd3f8fe48b934e34e473428
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2019-11-26 00:34:23 -0500

    (0x0D: fix) task 16
    Added missing ORDER BY clause.

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- Lists all records in 'second_table' which contain a 'name' value. Descending order.
-SELECT score, name FROM second_table WHERE name IS NOT NULL;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
