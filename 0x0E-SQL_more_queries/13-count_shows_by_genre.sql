9e5044e17f2e7a6b4d3ebee4b4e7d50be9a113c9
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2019-11-26 16:09:48 -0500

    (0x0E: refactor) task 13
    Changed keyword capitalization.

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- Lists all the shows that don't have a genre
-SELECT tv_genres.name, COUNT(*) as number_of_shows FROM ((tv_genres
+SELECT tv_genres.name, COUNT(*) AS number_of_shows FROM ((tv_genres
 	INNER JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id))
 	GROUP BY tv_show_genres.genre_id
 	ORDER BY number_of_shows DESC;
