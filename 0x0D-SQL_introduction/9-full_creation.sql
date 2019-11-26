74df5b5439d286753b4ce45a78490f16827c24ea
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2019-11-25 23:58:53 -0500

    (0x0D) task 9
    Created SQL script file for task 9.

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,17 @@
+-- Creates a table 'second_table' in the current database.
+-- The table contains:
+-- id, INT field. name, VARCHAR(256) field and score, INT field.
+-- The script will add four initial rows to the table.
+CREATE TABLE second_table (
+	id INT,
+	name VARCHAR(256),
+	score INT
+);
+
+-- Adds the four initial rows to 'second_table'
+INSERT INTO second_table (id, name, score)
+VALUES
+	(1, "John", 10),
+	(2, "Alex", 3),
+	(3, "Bob", 14),
+	(4, "George", 8);
