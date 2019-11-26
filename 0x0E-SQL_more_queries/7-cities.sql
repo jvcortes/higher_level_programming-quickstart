af5963d88dc552d9ec7fc4f92eb3a9a803e31f4c
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2019-11-26 18:12:24 +0000

    (0x0E: fix) task 7
    Fixed 'no database selected' problem.

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -4,6 +4,7 @@
 -- state_id, INT, foreign key that references 'id' from the 'states' table
 -- name, VARCHAR(256) not null
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	state_id INT NOT NULL,
