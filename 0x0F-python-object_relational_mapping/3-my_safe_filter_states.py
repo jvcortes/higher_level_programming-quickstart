ef36571c6b656da9c5e7dc09b32dea384c87b232
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2019-12-14 16:34:03 -0500

    (0x0F: fix) 3-my_safe_filter_states.py
    Changed condition for query.

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
 
     cur.execute("""
                     SELECT * FROM states
-                    WHERE name LIKE BINARY %(name)s
+                    WHERE name == %(name)s
                     ORDER BY id ASC;
                 """,
                 {'name': sys.argv[4]})
