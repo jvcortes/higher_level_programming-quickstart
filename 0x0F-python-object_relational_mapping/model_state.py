4df1facac23c523ad083781ddf9792e85781823b
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2019-12-14 18:48:15 -0500

    (0x0F) task 6
    Created class definition for task 6.

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This module contains the State class.
+"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    Defines a State.
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
