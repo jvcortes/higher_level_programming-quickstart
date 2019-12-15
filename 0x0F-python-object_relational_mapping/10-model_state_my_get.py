4844dd166cdda315393c61599568f736f579844f
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2019-12-14 20:17:25 -0500

    (0x0F: fix) task 10
    Fixed output formatting.

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -17,7 +17,11 @@ if __name__ == "__main__":
     Session.configure(bind=engine)
     session = Session()
 
-    for state in session.query(State).filter(
-            State.name == sys.argv[4]).order_by(State.id).all():
-        print("{}: {}".format(state.id, state.name))
+    state = session.query(State).filter(
+        State.name == sys.argv[4]).order_by(State.id).first()
+    if state:
+        print("{}".format(state.id))
+    else:
+        print("Not found")
+
     session.close()
