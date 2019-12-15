c50e6c43cd56499f8caac31423001f056b317f11
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2019-12-14 19:43:09 -0500

    (0x0F: fix) 7-model_state_fetch_all.py
    Added port 3306 to connection string.

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 # Shows all the State objects
-import sys
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
-
-
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+    import sys
+    from sqlalchemy import (create_engine)
+    from sqlalchemy.orm import sessionmaker
+    from model_state import Base, State
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1],
                                    sys.argv[2],
                                    sys.argv[3]),
