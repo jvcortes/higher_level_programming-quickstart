eb3bb750d7d91f65179ba4d6cda367a5aec34657
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2020-01-14 20:35:27 -0500

    (0x11: feat) task 5
    Improved script's capability to take the first argument.

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -5,7 +5,8 @@ header "X-Request-Id" using requests.Request
 """
 if __name__ == '__main__':
     import requests
+    import sys
 
-    request = requests.get("https://intranet.hbtn.io/status")
+    request = requests.get(sys.argv[1])
     if "X-Request-Id" in request.headers:
         print(request.headers["X-Request-Id"])
