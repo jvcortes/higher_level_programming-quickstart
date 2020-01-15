c7465522e58634eb64cd7cdcb59f35d179e99274
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2020-01-14 22:28:07 -0500

    (0x11) task 11
    Created Python script for task 11.

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Makes a HTTP POST request to https://api.github.com/repos with the repo owner
+as the first argument and the repo name as the second argument.
+"""
+if __name__ == '__main__':
+    import requests
+    import sys
+
+    request = requests.get("https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[1],
+        sys.argv[2]
+    ))
+    for commit in request.json()[:10]:
+        print("{}: {}".format(commit['sha'],
+                              commit['commit']['author']['name']))
