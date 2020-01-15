9949f735e046f5a3c5d7ca73d9311346f59ced73
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2020-01-14 21:46:09 -0500

    (0x11) task 9
    Created Python script for task 11.

---
+++ 0x11-python-network_1/9-starwars.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Makes a HTTP GET request to http://swapi.co/api/people/ with the
+parameter "search" as the first argument.
+"""
+if __name__ == '__main__':
+    import requests
+    import sys
+
+    request = requests.get("http://swapi.co/api/people/",
+                           params={'search': sys.argv[1]})
+    print("Number of results: {}".format(request.json()
+                                         ['count']))
+    for ch in request.json()['results']:
+        print(ch['name'])
