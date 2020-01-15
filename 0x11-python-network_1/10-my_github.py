7350d26cb6cbbab478a4a45206bf0a4ce51fae04
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2020-01-14 22:07:01 -0500

    (0x11) task 10
    Created Python script for task 11.

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Makes a HTTP POST request to https://api.github.com/user with the username
+as the first argument and its password as the seconf argument.
+"""
+if __name__ == '__main__':
+    import requests
+    import sys
+
+    request = requests.get("https://api.github.com/user", auth=(sys.argv[1],
+                                                                sys.argv[2]))
+    print(request.json()['id'])
