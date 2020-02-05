c1d2a5aac94eb423ad92b81f126d038381569841
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2020-02-04 19:11:56 -0500

    (0x15: refactor) task 6
    Implemented use of for...of loop.

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 // Prints 'C is fun', then prints 'Python is cool' and then, finally prints
 // "Javascript is amazing"
-['C is fun', 'Python is cool', 'Javascript is amazing'].forEach(
-  str => console.log(str));
+for (const str of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
+  console.log(str);
+}
