fa0d5a3a231f5cbd4dfcc17b7a0dea704645d385
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2020-02-04 20:46:10 -0500

    (0x12: fix) task 11
    Fixed case where script takes an array containing negative numbers.

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 // Prints the second biggest integer in its arguments.
 if (process.argv.length > 3) {
-  const sorted = process.argv.slice(2).map((val, index) => parseInt(val)).sort();
+  const sorted = process.argv.slice(2).map((val, index) => parseInt(val))
+    .sort((a, b) => a - b);
   console.log(sorted[sorted.length - 2]);
 } else {
   console.log(0);
