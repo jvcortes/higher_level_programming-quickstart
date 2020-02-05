c3ede9a9f2f73e166161f0eadce437501fc9ee0a
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2020-02-04 20:21:44 -0500

    (0x12: fix) task 11
    Fixed cases where the number of arguments were less than two.

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,4 +1,8 @@
 #!/usr/bin/node
 // Prints the second biggest integer in its arguments.
-const sorted = process.argv.slice(2).map((val, index) => parseInt(val)).sort();
-console.log(sorted.length > 1 ? sorted[sorted.length - 2] : sorted[0]);
+if (process.argv.length > 3) {
+  const sorted = process.argv.slice(2).map((val, index) => parseInt(val)).sort();
+  console.log(sorted[sorted.length - 2]);
+} else {
+  console.log(0);
+}
