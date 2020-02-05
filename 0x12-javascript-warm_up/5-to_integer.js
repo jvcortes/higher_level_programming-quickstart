b1f889f4064faae5e9eb7ced1db9fc99307cb173
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2020-02-04 19:07:23 -0500

    (0x12: fix) task 5
    Fixed output formatting.

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
 // Prints "My number: <first argument>"
 console.log(
-  parseInt(process.argv[2]) ? parseInt(process.argv[2]) : 'Not a number');
+  parseInt(process.argv[2]) ? 'My number: ' + parseInt(
+    process.argv[2]) : 'Not a number');
