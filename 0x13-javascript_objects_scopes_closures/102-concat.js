95a841bf797949ae9e7c7682c5b9b80b3670e5e2
Javier Felipe Feo Cortés <jfelipefc@gmail.com>
2020-02-11 10:53:34 -0500

    (0x13) task 13
    Created script for task 13.

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// Concatenates two files.
+const fs = require('fs');
+
+function generateFile(fileA, fileB, result) {
+  let file = "";
+  file = fs.readFileSync(fileA, 'utf8');
+  file += fs.readFileSync(fileB, 'utf8');
+  fs.writeFileSync(result, file);
+}
+
+generateFile(process.argv[2], process.argv[3], process.argv[4]);
